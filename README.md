5596d7d4bda6067d95222484082155f7abe46678
elwiz <tomek1982@gazeta.pl>
2013-10-25 09:41:15 +0200

    Beta version of the plugin.

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-require-hb
-==========
+# Require-HB
 
 Require.js plugin for the handlebars template parser.
+
+## Requirements
+
+* [Require.js](https://github.com/jrburke/requirejs)
+* [Underscore](https://github.com/jashkenas/underscore)
+* [Handlebars] (https://github.com/wycats/handlebars.js)
+
+## Installation
+
+Use the volo dependency manager by adding this repository to the dependency list.
+
+## Usage
+
+```javascript
+require.config({
+    baseUrl: 'js',
+    paths: {
+        Underscore: 'libs/underscore',
+        Handlebars: 'libs/handlebars',
+        hb: 'libs/require-plugins/hb'
+    },
+    shim: {
+        Underscore: {
+            deps: [],
+            exports: '_'
+        },
+        Handlebars: {
+            deps: [],
+            exports: 'Handlebars'
+        },
+        hb: {
+            deps: ['Handlebars']
+        }
+    },
+    hb: {
+        location: 'templates',
+        extension: 'hbs'
+    }
+});
+
+require(['hb!index'], function (index) {
+    'use strict';
+    window.document.body.innerHTML = index({foo: 'bar'});
+});
+```
+
+## Configuration
+
+The configuration of the hb plugin is not required. There is a default value for both options.
+
+* `location` - defaults to an empty string
+* `extension` - defaults to html
---
+++ hb.js
@@ -0,0 +1,44 @@
+/**
+ * Handlebars plugin for require.js
+ * @author elwiz
+ */
+define(['Underscore', 'Handlebars'], function (_, Handlebars) {
+    'use strict';
+
+    return {
+        load: function (name, parentRequire, onload, config) {
+
+            // default configuration
+            var hbc = {
+                location: '',
+                extension: 'html'
+            };
+
+            // import configuration from require
+            if (config.hb) {
+                hbc = _.extend(hbc, config.hb);
+            }
+
+            // use predefined template location
+            if (hbc.location) {
+                if (hbc.location.charAt(hbc.location.length - 1) !== '/') {
+                    hbc.location += '/';
+                }
+                name = hbc.location + name;
+            }
+
+            // use predefined template extension
+            if (hbc.extension) {
+                if (hbc.extension.charAt(0) !== '.') {
+                    hbc.extension = '.' + hbc.extension;
+                }
+                name += hbc.extension;
+            }
+
+            // use the text plugin to load the template source
+            parentRequire(['text!' + name], function (template) {
+                onload(Handlebars.compile(template));
+            });
+        }
+    };
+});
